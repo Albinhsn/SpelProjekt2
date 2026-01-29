2152d9a61c8d222b93e8e2ee188f39524b8234d0
MnaBeast <a24maxma@student.his.se>
2026-01-29 14:29:33 +0100

    Changes to MovementController
    Made movementController use InputAction instead of InputActionReference.

--- Spelprojekt2/Assets/Scripts/MovementController.cs
+++ Spelprojekt2/Assets/Scripts/MovementController.cs
@@ -9,28 +9,22 @@ public class MovementController : MonoBehaviour
     [SerializeField]
     private float m_speed;
 
-    [SerializeField]
-    private float m_jumpSpeed;
-
-    [SerializeField]
-    private InputActionReference m_movementAction;
-
     [SerializeField] 
-    private InputActionReference m_lookAction;
-
-    [SerializeField]
-    private InputActionReference m_jumpAction;
+    private float m_jumpSpeed;
 
     [SerializeField]
     [Range(0.1f, 10.0f)]
     private float m_mouseSensitivity;
-
     private Rigidbody m_rb;
     private bool m_isJumping;
+    private InputAction m_moveAction, m_lookAction, m_jumpAction;
 
     void Awake()
     {
         m_rb = GetComponent<Rigidbody>();
+        m_moveAction = GetComponent<PlayerInput>().actions["Move"];
+        m_lookAction = GetComponent<PlayerInput>().actions["Look"];
+        m_jumpAction = GetComponent<PlayerInput>().actions["Jump"];
     }
 
     private void OnTriggerEnter(Collider other)
@@ -40,9 +34,9 @@ public class MovementController : MonoBehaviour
     
     void Update()
     {
-        Vector2 input   = m_lookAction.action.ReadValue<Vector2>();
-        this.transform.Rotate(0, input.x * m_mouseSensitivity, 0);
-        if(m_jumpAction.action.WasPressedThisFrame() && !m_isJumping)
+        Vector2 input   = m_lookAction.ReadValue<Vector2>();
+        transform.Rotate(0, input.x * m_mouseSensitivity, 0);
+        if(m_jumpAction.WasPressedThisFrame() && !m_isJumping)
         {
             m_rb.linearVelocity += new Vector3(0, m_jumpSpeed, 0);
             m_isJumping = true;
@@ -52,14 +46,14 @@ public class MovementController : MonoBehaviour
     void FixedUpdate()
     {
         // HACK(ah): Really poor movement, pls swap
-        Vector3 forward = Rejection(this.transform.forward, new Vector3(0, 1, 0));
-        Vector3 right   = Rejection(this.transform.right, new Vector3(0, 1, 0));
-        Vector2 input   = m_movementAction.action.ReadValue<Vector2>();
+        Vector3 forward = Rejection(transform.forward, new Vector3(0, 1, 0));
+        Vector3 right   = Rejection(transform.right, new Vector3(0, 1, 0));
+        Vector2 input   = m_moveAction.ReadValue<Vector2>();
         Vector3 dir     = forward * input.y + right * input.x;
         if(dir.sqrMagnitude > 0)
         {
             dir = dir.normalized;
         }
-        this.transform.position = this.transform.position + m_speed * dir * Time.fixedDeltaTime;
+        transform.position = transform.position + m_speed * dir * Time.fixedDeltaTime;
     }
 }
