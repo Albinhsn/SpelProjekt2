b28e901ed8ccdea35b985d3f88da79e33ba5a6dc
MnaBeast <a24maxma@student.his.se>
2026-02-03 13:08:29 +0100

    Added decollider, and fixed transform rotation lerp

--- Spelprojekt2/Assets/Scripts/PlayerTransformRotation.cs
+++ Spelprojekt2/Assets/Scripts/PlayerTransformRotation.cs
@@ -1,7 +1,9 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerTransformRotation : MonoBehaviour
 {
+    [SerializeField] float m_transformRotationSpeed = .02f;
     private Rigidbody m_rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
@@ -12,6 +14,9 @@ public class PlayerTransformRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = Quaternion.LookRotation(m_rb.linearVelocity.normalized, Vector3.up);
+        if(m_rb.linearVelocity.sqrMagnitude > 0.01f)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(m_rb.linearVelocity.normalized, Vector3.up), m_transformRotationSpeed);
+        }
     }
 }
