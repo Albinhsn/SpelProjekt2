12bad4db6aa3f103d1276bf4e36a9f0bbc46e8cf
Albin Henningsson <albin.henningsson1@gmail.com>
2026-02-04 15:29:38 +0100

    removed redundant changes

--- Spelprojekt2/Assets/Scripts/GameStateData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "GameStateData", menuName = "Scriptable Objects/GameStateData")]
-public class GameStateData : ScriptableObject
-{
-    public int islandsUnlocked;
-
-}
