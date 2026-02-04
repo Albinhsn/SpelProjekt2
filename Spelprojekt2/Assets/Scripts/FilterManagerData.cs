3a049e424d9ade6b196477cb076a71ebee39d0ff
Albin Henningsson <albin.henningsson1@gmail.com>
2026-02-04 08:55:55 +0100

    removed filtermanagerdata

--- Spelprojekt2/Assets/Scripts/FilterManagerData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/FilterManagerData", order = 1)]
-public class FilterManagerData : ScriptableObject
-{
-    public FilterAction[] m_actions;
-}
