fcd18dce0efae603f87e57f3f1e9059a2e00ce96
MnaBeast <a24maxma@student.his.se>
2026-02-04 10:14:58 +0100

    Update PlayerTransformRotation.cs

--- Spelprojekt2/Assets/Scripts/PlayerTransformRotation.cs
+++ Spelprojekt2/Assets/Scripts/PlayerTransformRotation.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerTransformRotation : MonoBehaviour
 {
-    [SerializeField] float m_transformRotationSpeed = .02f;
+    [SerializeField] float m_transformRotationSpeed = .4f;
     private Rigidbody m_rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
