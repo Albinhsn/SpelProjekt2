f7c22ccb88b600506b4cc099e20c98862041026a
Aeresys <44092156+Aeresys@users.noreply.github.com>
2026-02-14 13:37:20 +0100

    Build-agnostic relay registration (does not work)

--- Spelprojekt2/Assets/Scripts/Interaction/Dialogue/DialogueReceiver.cs
+++ Spelprojekt2/Assets/Scripts/Interaction/Dialogue/DialogueReceiver.cs
@@ -28,14 +28,37 @@ namespace Interaction.Dialogue
         //Per-interaction
         [CanBeNull] private Story m_activeStory;
         private string m_speakerName;
+        private bool m_subscribed;
 
-        void Awake()
+        private void Awake()
         {
-            m_relay.d_onDialogueInitiation += InitiateDialogue;
-            m_relay.d_onDialogueUpdate += UpdateDialogue;
-            m_relay.d_onDialogueExit += EndDialogue;
-            m_dialogueContainer.SetActive(false);
-            if (m_altButtons is null) m_altButtons = new List<Button>();
+            m_subscribed = false;
+        }
+
+        void Start()
+        {
+            if (!m_subscribed)
+            {
+                m_relay.d_onDialogueInitiation += InitiateDialogue;
+                m_relay.d_onDialogueUpdate += UpdateDialogue;
+                m_relay.d_onDialogueExit += EndDialogue;
+                m_dialogueContainer.SetActive(false);
+                if (m_altButtons is null) m_altButtons = new List<Button>();
+                m_subscribed = true;
+            }
+        }
+
+        private void OnEnable()
+        {
+            if (!m_subscribed)
+            {
+                m_relay.d_onDialogueInitiation += InitiateDialogue;
+                m_relay.d_onDialogueUpdate += UpdateDialogue;
+                m_relay.d_onDialogueExit += EndDialogue;
+                m_dialogueContainer.SetActive(false);
+                if (m_altButtons is null) m_altButtons = new List<Button>();
+                m_subscribed = true;
+            }
         }
 
         void OnDestroy()
@@ -43,6 +66,7 @@ namespace Interaction.Dialogue
             m_relay.d_onDialogueInitiation -= InitiateDialogue;
             m_relay.d_onDialogueUpdate -= UpdateDialogue;
             m_relay.d_onDialogueExit -= EndDialogue;
+            m_subscribed = false;
         }
 
 #if false
