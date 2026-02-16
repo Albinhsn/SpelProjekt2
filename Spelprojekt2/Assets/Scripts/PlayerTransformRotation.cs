fa964ae17041d5811e33a23844705d13a3699ce3
MnaBeast <a24maxma@student.his.se>
2026-02-16 11:52:57 +0100

    Fixes to transform rotaion and added door to tutorial scenes

--- Spelprojekt2/Assets/Scripts/PlayerTransformRotation.cs
+++ Spelprojekt2/Assets/Scripts/PlayerTransformRotation.cs
@@ -5,13 +5,16 @@ using static LinAlg.LinAlg;
 public class PlayerTransformRotation : MonoBehaviour
 {
     [SerializeField,Tooltip("The speed at which the player rotates to face the direction of movement")] float m_transformRotationSpeed = .4f;
-
+    private Transform m_camera;
+    void Awake()
+    {
+        m_camera = GetComponentInChildren<CinemachineCamera>().transform;
+    }
     void FixedUpdate()
     {        
-        Transform camera = FindFirstObjectByType<CinemachineCamera>().transform;
 
-        Vector3 forward = Rejection(camera.forward, new Vector3(0, 1, 0));
-        Vector3 right   = Rejection(camera.right, new Vector3(0, 1, 0));
+        Vector3 forward = Rejection(m_camera.forward, new Vector3(0, 1, 0));
+        Vector3 right   = Rejection(m_camera.right, new Vector3(0, 1, 0));
         Vector2 input   = InputManager.ReadMovementValue();
         Vector3 dir     = forward * input.y + right * input.x;
 
