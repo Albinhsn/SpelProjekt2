f25156b442cc4e9df6e8892b421a77ca3d7c3f15
Aeresys <44092156+Aeresys@users.noreply.github.com>
2026-02-16 15:09:22 +0100

    SerializableObject persistent ID hash

--- Spelprojekt2/Assets/Scripts/SerializableObject.cs
+++ Spelprojekt2/Assets/Scripts/SerializableObject.cs
@@ -1,17 +1,46 @@
 using UnityEngine;
 using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
 
 public class SerializableObject : MonoBehaviour
 {
-    public Guid m_ID;
+    private Guid m_guid;
+    public Guid m_ID => m_guid;
 
-#if UNITY_EDITOR
-    public void OnValidate()
+    private void Awake()
     {
-        if(m_ID == null)
+        List<byte> data = new List<byte>(16);
+            
+        data.Add((byte) gameObject.scene.buildIndex);
+        foreach (char obj in gameObject.name)
         {
-            m_ID = System.Guid.NewGuid();
+            data.AddRange(BitConverter.GetBytes(obj));
         }
+        
+        data.AddRange(BitConverter.GetBytes(transform.localToWorldMatrix.m00));
+        data.AddRange(BitConverter.GetBytes(transform.localToWorldMatrix.m01));
+        data.AddRange(BitConverter.GetBytes(transform.localToWorldMatrix.m02));
+        data.AddRange(BitConverter.GetBytes(transform.localToWorldMatrix.m03));
+        data.AddRange(BitConverter.GetBytes(transform.localToWorldMatrix.m10));
+        data.AddRange(BitConverter.GetBytes(transform.localToWorldMatrix.m11));
+        data.AddRange(BitConverter.GetBytes(transform.localToWorldMatrix.m12));
+        data.AddRange(BitConverter.GetBytes(transform.localToWorldMatrix.m13));
+        data.AddRange(BitConverter.GetBytes(transform.localToWorldMatrix.m20));
+        data.AddRange(BitConverter.GetBytes(transform.localToWorldMatrix.m21));
+        data.AddRange(BitConverter.GetBytes(transform.localToWorldMatrix.m22));
+        data.AddRange(BitConverter.GetBytes(transform.localToWorldMatrix.m23));
+        data.AddRange(BitConverter.GetBytes(transform.localToWorldMatrix.m30));
+        data.AddRange(BitConverter.GetBytes(transform.localToWorldMatrix.m31));
+        data.AddRange(BitConverter.GetBytes(transform.localToWorldMatrix.m32));
+        data.AddRange(BitConverter.GetBytes(transform.localToWorldMatrix.m33));
+        
+
+        using (MD5 md5 = MD5.Create())
+        {
+            m_guid = new Guid(md5.ComputeHash(data.ToArray()));
+        }
+        
+        Debug.Log(m_guid.ToString());
     }
-#endif
 }
