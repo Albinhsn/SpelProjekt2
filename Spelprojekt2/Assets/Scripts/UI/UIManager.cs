953339d62487b598821119bcec6ec38ec22847ba
MnaBeast <a24maxma@student.his.se>
2026-02-17 17:09:29 +0100

    Update UIManager.cs

--- Spelprojekt2/Assets/Scripts/UI/UIManager.cs
+++ Spelprojekt2/Assets/Scripts/UI/UIManager.cs
@@ -289,12 +289,6 @@ public class UIManager : MonoBehaviour
         GUIStyle style  = new(GUI.skin.button);
         style.fontSize -= 10;
         style.font      = font;
-        
-        GUIStyle selected_style = new(GUI.skin.button);
-        selected_style.fontSize = style.fontSize;
-        selected_style.font = font;
-        selected_style.normal = selected_style.active;
-        selected_style.hover = selected_style.active;
 
         GUILayout.BeginHorizontal();
         GUILayout.FlexibleSpace();
@@ -309,17 +303,8 @@ public class UIManager : MonoBehaviour
 
         GUILayout.BeginHorizontal();
         GUILayout.FlexibleSpace();
-        int new_value  = prev_value;  //GUILayout.SelectionGrid(prev_value, selections, (int)FilterColor.COUNT, style);
-        for(int i = 0; i < (int)FilterColor.COUNT; i++)
-        {
-            bool pressed = GUILayout.Button(selections[i], i == prev_value ? selected_style : style);
-            Rect btn_rect = GUILayoutUtility.GetLastRect();
-            pressed = pressed || (btn_rect.Contains(Event.current.mousePosition) && InputManager.SelectUIOption());
-            if(pressed)
-            {
-                new_value = i;
-            }
-        }
+        int new_value = GUILayout.SelectionGrid(prev_value, selections, (int)FilterColor.COUNT, style);
+        
         GUILayout.FlexibleSpace();
         GUILayout.EndHorizontal();
 
