8c07b549d0807bb5a93c6204637e8a29c82d482d
Albinhsn <89841505+Albinhsn@users.noreply.github.com>
2026-02-17 15:08:24 +0100

    set cutscene music parameter (#116)
    * set cutscene music parameter
    
    * added object to set cutscene music and guard for none valid input

---
+++ Spelprojekt2/Assets/Scripts/SetCutsceneMusicParameter.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using AudioKit.FMOD;
+
+public class SetCutsceneMusicParameter : MonoBehaviour
+{
+    [SerializeField] private string m_parameter;
+    [SerializeField] private string m_eventID;
+
+    public void SetGlobalParameter()
+    {
+        if(!string.IsNullOrEmpty(m_parameter) && !string.IsNullOrEmpty(m_parameter))
+        {
+            var hub = AudioEventHub.I;
+            if(hub != null) hub.SetEventParam(m_eventID, m_parameter, 1.0f);
+        }
+
+        Destroy(this.gameObject);
+    }
+}
