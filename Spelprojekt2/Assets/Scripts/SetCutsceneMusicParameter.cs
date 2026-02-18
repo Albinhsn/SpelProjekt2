2219012bc74df6ea4b9250a6dea53f392c9526b6
Albinhsn <89841505+Albinhsn@users.noreply.github.com>
2026-02-18 12:11:19 +0100

    door sound and tutorial reveal event set correctly (#128)
    * wip door sound, no sound exists yet
    
    * added door sound
    
    * use correct door sound
    
    * set correct param for tutorial music

--- Spelprojekt2/Assets/Scripts/SetCutsceneMusicParameter.cs
+++ Spelprojekt2/Assets/Scripts/SetCutsceneMusicParameter.cs
@@ -6,9 +6,9 @@ public class SetCutsceneMusicParameter : MonoBehaviour
     [SerializeField] private string m_parameter;
     [SerializeField] private string m_eventID;
 
-    public void SetGlobalParameter()
+    public void SetEventParameter()
     {
-        if(!string.IsNullOrEmpty(m_parameter) && !string.IsNullOrEmpty(m_parameter))
+        if(!string.IsNullOrEmpty(m_eventID) && !string.IsNullOrEmpty(m_parameter))
         {
             var hub = AudioEventHub.I;
             if(hub != null) hub.SetEventParam(m_eventID, m_parameter, 1.0f);
