d5a46f28b0db55c917198e8be48aacc0b925331b
Albin Henningsson <albin.henningsson1@gmail.com>
2026-02-19 10:29:39 +0100

    finished snapshot

--- Spelprojekt2/Assets/Scripts/LevelCheckpointManager.cs
+++ Spelprojekt2/Assets/Scripts/LevelCheckpointManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public sealed class LevelCheckpointManager
 {
+    public static bool m_allowChangeCheckpoint = true;
     public static Vector3 m_currentSpawnPointPosition;
     public static Quaternion m_currentSpawnPointRotation;
     public static int m_sceneBuildIndex;
@@ -17,11 +18,29 @@ public sealed class LevelCheckpointManager
         }
     }
 
+    public static void SetFirstSpawnPoint()
+    {
+        LevelCheckpoint[] checkpoints = Object.FindObjectsByType<LevelCheckpoint>(FindObjectsSortMode.None);
+
+        for(int i = 0; i < checkpoints.Length; i++)
+        {
+            var checkpoint = checkpoints[i];
+            if(checkpoint.m_isFirstCheckpointOfLevel)
+            {
+                SetNewSpawnPoint(checkpoint.transform.position, checkpoint.transform.rotation,
+                      checkpoint.gameObject.scene.buildIndex);
+            }
+        }
+    }
+
     public static void SetNewSpawnPoint(Vector3 p, Quaternion r, int scene_build_index)
     {
-        m_currentSpawnPointPosition = p;
-        m_currentSpawnPointRotation = r;
-        m_sceneBuildIndex           = scene_build_index;
+        if(m_allowChangeCheckpoint)
+        {
+            m_currentSpawnPointPosition = p;
+            m_currentSpawnPointRotation = r;
+            m_sceneBuildIndex           = scene_build_index;
+        }
     }
 
     public static void ResetToCheckpoint()
