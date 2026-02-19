6b1e5dc562d8e6dd5acac270be5d535abe306ab4
Albin Henningsson <albin.henningsson1@gmail.com>
2026-02-19 12:17:55 +0100

    reset filter on delete save

--- Spelprojekt2/Assets/Scripts/UI/UIManager.cs
+++ Spelprojekt2/Assets/Scripts/UI/UIManager.cs
@@ -375,6 +375,7 @@ public class UIManager : MonoBehaviour
                 if(MenuBtn("Delete save", btn_index++))
                 {
                     PersistentDataManager.RemoveAllSerializedData();
+                    FilterManager.m_filterUnlocked = false;
                 }
 
                 GUILayout.Space(25);
