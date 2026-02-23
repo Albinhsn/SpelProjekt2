c748640a2b32ecaec78970ad1ff0ab2fdac73c18
Albin Henningsson <albin.henningsson1@gmail.com>
2026-02-23 14:39:30 +0100

    added velocity parameter for box sound on collision

--- Spelprojekt2/Assets/Scripts/BoxSoundOnCollision.cs
+++ Spelprojekt2/Assets/Scripts/BoxSoundOnCollision.cs
@@ -10,7 +10,6 @@ public class BoxSoundOnCollision : MonoBehaviour
     [Range(0.1f, 100.0f)]
     private float m_maxVelocity = 10.0f;
 
-    private FMOD.Studio.EventInstance m_soundInstance;
     private Rigidbody m_rb;
 
     void Awake()
@@ -18,22 +17,17 @@ public class BoxSoundOnCollision : MonoBehaviour
         m_rb = GetComponent<Rigidbody>();
     }
 
-    void OnDestroy()
-    {
-    }
-
     void OnCollisionEnter(Collision other)
     {
         if(other.impulse.sqrMagnitude > 0)
         {
-            Debug.Log("aa");
-            m_soundInstance = RuntimeManager.CreateInstance(m_audioCue.evt);
-            m_soundInstance.set3DAttributes(RuntimeUtils.To3DAttributes(this.transform.position));
+            var si = RuntimeManager.CreateInstance(m_audioCue.evt);
+            si.set3DAttributes(RuntimeUtils.To3DAttributes(this.transform.position));
 
             float ratio    = Mathf.Clamp(m_rb.linearVelocity.magnitude / m_maxVelocity, 0, 1);
-            FMOD.RESULT ok = m_soundInstance.setParameterByName("velocity", ratio);
-            ok = m_soundInstance.start();
-            ok = m_soundInstance.release();
+            FMOD.RESULT ok = si.setParameterByName("velocity", ratio);
+            ok = si.start();
+            ok = si.release();
         }
     }
 }
