6d3113e97808aa0461a4e1ddbdea5a964445e2d7
Aeresys <44092156+Aeresys@users.noreply.github.com>
2026-02-26 11:43:42 +0100

    Holdable movement and collision fixes
    next up: flight prevention

--- Spelprojekt2/Assets/Scripts/Interaction/Holdable.cs
+++ Spelprojekt2/Assets/Scripts/Interaction/Holdable.cs
@@ -10,18 +10,21 @@ namespace Interaction
         [SerializeField] private float m_holdDistance;
         [SerializeField] private float m_linearMovementThreshold;
         [SerializeField] private float m_maxVelocity;
+        [SerializeField] private float m_acceleration = 30;
         [SerializeField] private float m_deceleration;
         [SerializeField] private float m_snapThreshold;
+        private float m_forwardLinearArc = 0.1f;
         
         private float m_currentVelocity;
         private Vector3 m_targetPosition => m_activeInteractor.position + m_activeInteractor.aimDirection * m_holdDistance;
         private Vector3 m_linearTargetDirection => (m_targetPosition - transform.position).normalized;
         private float m_targetDistance => Vector3.Distance(transform.position, m_targetPosition);
         private Vector3 m_interactorDirection => (m_activeInteractor.position - transform.position).normalized;
-        private float m_distanceToInteractor => Vector3.Distance(transform.position, m_activeInteractor.position);
+        private float m_distanceToInteractor => Vector3.Distance(transform.position, m_activeInteractor.position);//TODO: capsule
         private float m_outerDistance => m_distanceToInteractor - m_holdDistance;
         private float m_decThreshold => MathF.Pow(m_currentVelocity, 2) / (2 * m_deceleration);
         private Vector3 m_sphericalTargetDirection => (m_linearTargetDirection - Vector3.Dot(m_interactorDirection, m_linearTargetDirection) * m_interactorDirection).normalized;
+        private float m_forwardAngleCorrespondence => Vector3.Dot(m_activeInteractor.aimDirection, (transform.position - m_activeInteractor.position).normalized);
 
 
         private Rigidbody m_rb;
@@ -38,17 +41,20 @@ namespace Interaction
         {
             if (m_isHeld)
             {
-                m_isHeld = false;
                 m_activeInteractor.FinishInteraction();
-                m_rb.useGravity = true;
+                m_rb.collisionDetectionMode = m_savedCollissionDetectionMode;
                 m_activeInteractor = null;
+                m_isHeld = false;
             }
             base.VirtOnDisable();
         }
 
+        private CollisionDetectionMode m_savedCollissionDetectionMode;
         public override void Interact(Interactor interactor)
         {
-            m_rb.useGravity = false;
+            //m_rb.useGravity = false;
+            m_savedCollissionDetectionMode = m_rb.collisionDetectionMode;
+            m_rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
             m_activeInteractor = interactor;
             m_isHeld = true;
             m_currentVelocity = 0;
@@ -56,7 +62,8 @@ namespace Interaction
 
         public override bool TryCancelInteract(Interactor interactor)
         {
-            m_rb.useGravity = true;
+            //m_rb.useGravity = true;
+            m_rb.collisionDetectionMode = m_savedCollissionDetectionMode;
             m_activeInteractor = null;
             m_isHeld = false;
             return true;
@@ -66,16 +73,21 @@ namespace Interaction
         {
             if (m_isHeld)
             {
-                if (m_targetDistance < m_maxVelocity * Time.fixedDeltaTime)
+                //Update velocity
+                m_currentVelocity = m_targetDistance > m_decThreshold ? MathF.Min(m_maxVelocity, m_rb.linearVelocity.magnitude + m_acceleration * Time.fixedDeltaTime) : MathF.Max(0, m_currentVelocity - m_deceleration * Time.fixedDeltaTime);
+                
+                if (m_targetDistance < m_snapThreshold + m_currentVelocity * Time.fixedDeltaTime)//Snap
                 {
-                    m_currentVelocity = 0;
-                    m_rb.linearVelocity = Vector3.zero;
-                    m_rb.MovePosition(m_targetPosition);
+                    m_currentVelocity = m_targetDistance / Time.fixedDeltaTime;
+                    m_rb.linearVelocity = m_linearTargetDirection * m_currentVelocity;
                     return;
                 }
                 
-                //Update velocity
-                m_currentVelocity = m_targetDistance > m_decThreshold ? m_maxVelocity : MathF.Max(0, m_currentVelocity - m_deceleration * Time.fixedDeltaTime);
+                if (m_forwardAngleCorrespondence > 1 - m_forwardLinearArc)
+                {
+                    m_rb.linearVelocity = m_linearTargetDirection * m_currentVelocity;
+                    return;
+                }
                 
                 m_rb.linearVelocity = (
                     Vector3.Slerp(m_sphericalTargetDirection, m_linearTargetDirection, //Slerp input
@@ -87,19 +99,32 @@ namespace Interaction
                     * -m_interactorDirection 
                 
                     ) * m_currentVelocity; //Velocity scale
+                
+                
+                //Wall clip prevention (unnecessary, now using continuous collision detection while held
+                /*RaycastHit[] hit = Physics.RaycastAll(transform.position, m_rb.linearVelocity.normalized,
+                    m_currentVelocity * Time.deltaTime, srUtils.Unity.Utils.GetPhysicsLayerMask(gameObject.layer));
+                for (int a = 0; a < hit.Length; a++)
+                {
+                    if (hit[a].rigidbody != m_rb) //Wall detected
+                    {
+                        
+                        break;
+                    }
+                }*/
             }
         }
 
         private void OnDrawGizmos()
         {
             if(!m_isHeld) return;
-
             
             Gizmos.color = Color.magenta;
             Gizmos.DrawSphere(m_targetPosition, m_snapThreshold); //Object target
             
             Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(m_activeInteractor.position, m_holdDistance); //Min distance
+            Gizmos.DrawWireSphere(m_activeInteractor.position, m_holdDistance); //Min distance on interactor
+            Gizmos.DrawWireSphere(transform.position, m_holdDistance); //Min distance on object
             Gizmos.color = Color.green;
             Gizmos.DrawWireSphere(m_activeInteractor.position, m_linearMovementThreshold); //Linear movement threshold
 
@@ -110,11 +135,20 @@ namespace Interaction
 
             Gizmos.color = Color.green;
             Gizmos.DrawLine(transform.position, transform.position + (
-                Vector3.Slerp(m_sphericalTargetDirection, m_linearTargetDirection,//Slerp input
+                Vector3.Slerp(m_sphericalTargetDirection, m_linearTargetDirection, //Slerp input
                     MathF.Max(0, MathF.Min(1, m_outerDistance / m_linearMovementThreshold))) //Slerp t
-                    
-                //+ MathF.Max(0, m_outerDistance) * -m_interactorDirection //Move out from hold range
+
+                + (m_outerDistance > 0//Move out from hold range
+                    ? 0
+                    : (1 - m_distanceToInteractor / m_holdDistance) * m_maxVelocity)
+                * -m_interactorDirection
             )); //Active direction
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, m_holdDistance); //Min distance on object
         }
     }
 }
---
+++ Spelprojekt2/Assets/Scripts/srUtils/Unity/Utils.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace srUtils.Unity
+{
+    public static class Utils
+    {
+	    public static LayerMask GetPhysicsLayerMask(int layer)
+	    {
+		    int output = 0;
+		    for (int a = 0; a < 32; a++)
+		    {
+			    if (Physics.GetIgnoreLayerCollision(layer, a)) output |= 1 << a;
+		    }
+		    return output;
+	    }
+    }
+}
