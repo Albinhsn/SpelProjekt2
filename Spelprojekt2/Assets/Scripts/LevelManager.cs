4bb818527ecf648e44c712a52f49b33b20e25e38
albin hsn <albin.henningsson1@gmail.com>
2026-03-10 13:55:04 +0100

    fix build error

--- Spelprojekt2/Assets/Scripts/LevelManager.cs
+++ Spelprojekt2/Assets/Scripts/LevelManager.cs
@@ -21,14 +21,14 @@ public sealed class LevelManager
     LevelManager()
     {
         m_glitchAudio = Resources.Load<AudioCueSO>("Audio/AC_Glitch");
-        m_duckSnapshot = Resources.Load<AudioCueSO>("Audio/AC_Duck");
+        m_duckSnapshot = "MusicDuck";
     }
 
     private LevelData m_currentLevel;
     private LevelData m_nextLevel;
 
     private AudioCueSO m_glitchAudio;
-    private AudioCueSO m_duckSnapshot;
+    private string m_duckSnapshot;
     private EventInstance m_soundInstance;
 
     private SceneLoader m_sceneLoader;
@@ -72,7 +72,7 @@ public sealed class LevelManager
             var I = AudioSystem.I;
             if(I != null)
             {
-                I.SetSnapshot(Instance.m_duckSnapshot.evt.Path, true, 0);
+                I.SetSnapshot(Instance.m_duckSnapshot, true, 0);
             }
         }
 
@@ -164,7 +164,7 @@ public sealed class LevelManager
             var I = AudioSystem.I;
             if(I != null)
             {
-                I.SetSnapshot(Instance.m_duckSnapshot.evt.Path, false, 0);
+                I.SetSnapshot(Instance.m_duckSnapshot, false, 0);
             }
         }
 
