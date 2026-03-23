145f51e43abcc965a6be099d468b1e05f25f752e
Albinhsn <89841505+Albinhsn@users.noreply.github.com>
2026-03-23 21:45:51 +0100

    on death audio

--- Spelprojekt2/Assets/Scripts/GlitchRespawnOnTrigger.cs
+++ Spelprojekt2/Assets/Scripts/GlitchRespawnOnTrigger.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using AudioKit.FMOD;
 
 [RequireComponent(typeof(Collider))]
 public class GlitchRespawnOnTrigger : MonoBehaviour
 {
+    [SerializeField]
+    private AudioCueSO m_onDeathAudioCue;
 
     void RespawnPlayer()
     {
@@ -13,10 +16,12 @@ public class GlitchRespawnOnTrigger : MonoBehaviour
     {
         if(other.tag == "Player")
         {
-            if(GlitchTransitionManager.StartTransition(2.0f, 0.0f))
+            if(GlitchTransitionManager.StartTransition(2.5f, 0.0f))
             {
                 GlitchTransitionManager.m_onTransitionEnd.AddListener(RespawnPlayer);
             }
+
+            SfxDirector.PlayCue2(m_onDeathAudioCue, Vector3.zero);
         }
 
     }
